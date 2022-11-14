b93d25c7173cb294bcba9a4f452e70688ec3b509
INTEXSTORE2 <117637675+INTEXSTORE2@users.noreply.github.com>
2022-11-15 00:06:26 +0700

    Add files via upload

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,13 +4,13 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtu.be/1SQmGPpd0kU*`
+let str = `*Minta ke owner klk di kasih kamu beruntung :v*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Jarotr_",
-    title: 'Oscar-MultiDevice',
-    body: 'Jangan Dijual ngentod Jangan Lupa Suscribe',
+    mediaUrl: "https://chat.whatsapp.com/JUasnSIppUK8WL1BSHWvX3",
+    title: '𝙎𝙚𝙞𝙠𝙖 𝘽𝙤𝙩𝙯𝙯',
+    body: 'Join gc ngab :v',
     thumbnail: thumb,
     sourceUrl: sig
   }
